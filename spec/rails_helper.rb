b9154434d0e845ea30a2fead0caa2f884d4e184f
phuongpham167 <phuongptt.uet@gmail.com>
2018-09-24 10:24:55 +0700

    part one

---
+++ spec/rails_helper.rb
@@ -0,0 +1,41 @@
+# require database cleaner at the top level
+require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require "rspec/rails"
+require "database_cleaner"
+
+# [...]
+# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+# [...]
+RSpec.configuration do |config|
+  config.include RequestSpecHelper, type: :request
+  # [...]
+  # add `FactoryBot` methods
+  config.include FactoryBot::Syntax::Methods
+
+  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  # start the transaction strategy as examples are run
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+  # [...]
+end
